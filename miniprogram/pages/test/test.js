7159d56e3dad5e30a880ba96e83de47393e1a454
Dream <13866131255@163.com>
2020-04-14 22:31:34 +0800

    抽奖页面样式

--- miniprogram/pages/test/test.js
+++ miniprogram/pages/test/test.js
@@ -1,18 +1,39 @@
-// miniprogram/pages/test/test.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    lottery_info:{
+      title: '巫师3',
+      desc: '满600人即刻开奖',
+      img: 'https://media.st.dl.eccdnx.com/steam/apps/292030/header.jpg?t=1581375222',
+      attended: [
+        'https://wx.qlogo.cn/mmopen/vi_32/n3E9esUk5rpibaOVus5OLMBuwzqtXQkmm00VfpCVj8zS9X8kVzMVWZzT2phUadVan3wrj6yRqWiaZC1AGoolC2ibA/132',
+        'https://thirdwx.qlogo.cn/mmopen/vi_32/Q3auHgzwzM4pfOnQ0H4YzgQpjYw5mBxabDFhnN9UzOFvhOCD1qhRLiaGyiagVHLs3KMgxXE7Niciccmerb7LBggUkw/132',
+        'https://wx.qlogo.cn/mmopen/vi_32/Ee3rJFn8tPxGwq8Vlzr0OaUfnbUHyCJt2wFzFTFgI7iaIg42hrfaaIPPRtAvXqL3Gclrl2Du9RTSXtnvIDSOlCg/132',
+        'https://wx.qlogo.cn/mmopen/vi_32/8p9dyJCvCMccsjVomaP9C4x4OWia1siajjRlLb9N0ZUPudfRjGUholbm2ickloEWIMexKU8gepPib7tImEG9h7suYw/132'
+      ]
+    },
+    lottery_self_info:{
+      isComplete: true,
+      invited: [
+        'https://wx.qlogo.cn/mmopen/vi_32/n3E9esUk5rpibaOVus5OLMBuwzqtXQkmm00VfpCVj8zS9X8kVzMVWZzT2phUadVan3wrj6yRqWiaZC1AGoolC2ibA/132',
+        'https://thirdwx.qlogo.cn/mmopen/vi_32/Q3auHgzwzM4pfOnQ0H4YzgQpjYw5mBxabDFhnN9UzOFvhOCD1qhRLiaGyiagVHLs3KMgxXE7Niciccmerb7LBggUkw/132',
+        'https://wx.qlogo.cn/mmopen/vi_32/Ee3rJFn8tPxGwq8Vlzr0OaUfnbUHyCJt2wFzFTFgI7iaIg42hrfaaIPPRtAvXqL3Gclrl2Du9RTSXtnvIDSOlCg/132',
+        'https://wx.qlogo.cn/mmopen/vi_32/Ee3rJFn8tPxGwq8Vlzr0OaUfnbUHyCJt2wFzFTFgI7iaIg42hrfaaIPPRtAvXqL3Gclrl2Du9RTSXtnvIDSOlCg/132',
+        'https://wx.qlogo.cn/mmopen/vi_32/Ee3rJFn8tPxGwq8Vlzr0OaUfnbUHyCJt2wFzFTFgI7iaIg42hrfaaIPPRtAvXqL3Gclrl2Du9RTSXtnvIDSOlCg/132',
+        'https://wx.qlogo.cn/mmopen/vi_32/Ee3rJFn8tPxGwq8Vlzr0OaUfnbUHyCJt2wFzFTFgI7iaIg42hrfaaIPPRtAvXqL3Gclrl2Du9RTSXtnvIDSOlCg/132',
+        'https://wx.qlogo.cn/mmopen/vi_32/8p9dyJCvCMccsjVomaP9C4x4OWia1siajjRlLb9N0ZUPudfRjGUholbm2ickloEWIMexKU8gepPib7tImEG9h7suYw/132'
+      ]
+    },
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(options.openId)
   },
 
   /**
@@ -61,6 +82,9 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    return {
+      title: this.data.lottery_info.title,
+      path: '/pages/test/test?openId=123'
+    }
   }
 })
